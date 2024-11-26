2f9c83f470eda5a91f0ca45bf0ba90a0e97902bd
tucno21 <carlitostucno@gmail.com>
2024-11-26 06:14:46 -0500

    actualizacion de react router dom

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import ErrorPage from "../pages/error/ErrorPage";
 import HomeLayout from "../layout/HomeLayout";
 import IndexPage from "../pages/home/IndexPage";
